f98d6f7fb9dd12272b20d18fc772607f3a018a16
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2024-11-18 13:26:06 -0600

    additional functionality for the edit and delete task buttons

--- 2024_JS/TodoApp/script.js
+++ 2024_JS/TodoApp/script.js
@@ -24,6 +24,8 @@ const addOrUpdateTask = () => {
 
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
     }
 
     updateTaskContainer();
@@ -31,6 +33,8 @@ const addOrUpdateTask = () => {
 };
 
 const updateTaskContainer = () => {
+    tasksContainer.innerHTML = "";
+
     taskData.forEach(
         ({id, title, date, description}) => {
             tasksContainer.innerHTML += ` 
@@ -38,12 +42,37 @@ const updateTaskContainer = () => {
                 <p><strong>Title:</strong> ${title}</p>
                 <p><strong>Date:</strong> ${date}</p>
                 <p><strong>Description:</strong> ${description}</p>
-                <button type="button" class="btn">Edit</button>
-                <button type="button" class="btn">Delete</button>
+                <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+                <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
             </div>
             `
         }
     );
+};
+
+
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item)=> item.id = buttonEl.parentElement.id
+    );
+
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
+    currentTask = taskData[dataArrIndex];
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    taskForm.classList.toggle("hidden");
 }
 
 const reset = () => {
