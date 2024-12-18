41edcb149d38425d782d4919f061ba4335aa55a3
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2024-12-17 23:50:49 -0600

    Todo App Complete

--- 2024_JS/TodoApp/script.js
+++ 2024_JS/TodoApp/script.js
@@ -10,16 +10,31 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
+const removeSpecialChars = (val) => {
+    return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
+}
+/** My attempt to line 16 */
+// function removeSpecialChars () {
+//     if (typeof str !== "string") {
+//         throw new Error("Input must be a string")
+//     }
+//     return str.replace(/[^a-zA-Z0-9 ]/g, "");
+// }
+
 const addOrUpdateTask = () => {
+    if (!titleInput.value.trim()){
+        alert("Please provide a title");
+        return;
+    }
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
+        id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: removeSpecialChars(titleInput.value),
         date: dateInput.value,
-        description: descriptionInput.value
+        description: removeSpecialChars(descriptionInput.value)
     }
 
     if (dataArrIndex === -1) {
@@ -27,7 +42,8 @@ const addOrUpdateTask = () => {
     } else {
         taskData[dataArrIndex] = taskObj;
     }
-
+    
+    localStorage.setItem("data",JSON.stringify(taskData));
     updateTaskContainer();
     reset();
 };
@@ -37,15 +53,15 @@ const updateTaskContainer = () => {
 
     taskData.forEach(
         ({id, title, date, description}) => {
-            tasksContainer.innerHTML += ` 
+            (tasksContainer.innerHTML += ` 
             <div class="task" id="${id}">
                 <p><strong>Title:</strong> ${title}</p>
                 <p><strong>Date:</strong> ${date}</p>
                 <p><strong>Description:</strong> ${description}</p>
-                <button type="button" class="btn" onclick="editTask(this)">Edit</button>
-                <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+                <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+                <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
             </div>
-            `
+            `)
         }
     );
 };
@@ -53,11 +69,12 @@ const updateTaskContainer = () => {
 
 const deleteTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex(
-        (item)=> item.id = buttonEl.parentElement.id
+        (item)=> item.id === buttonEl.parentElement.id
     );
 
     buttonEl.parentElement.remove();
     taskData.splice(dataArrIndex, 1);
+    localStorage.setItem("data",JSON.stringify(taskData));
 }
 
 const editTask = (buttonEl) => {
@@ -77,19 +94,23 @@ const editTask = (buttonEl) => {
 }
 
 const reset = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
     titleInput.value = "";
     dateInput.value = "";
-    descriptionInput = "";
+    descriptionInput.value = "";
     taskForm.classList.toggle("hidden");
     currentTask = {};
 };
 
+if(taskData.length) {
+    updateTaskContainer()
+}
 openTaskFormBtn.addEventListener("click", () => 
     taskForm.classList.toggle("hidden")
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-    const formInputsContainValues = title.value || dateInput.value || descriptionInput.value;
+    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
     const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
     if (formInputsContainValues && formInputValuesUpdated) {
@@ -112,12 +133,25 @@ taskForm.addEventListener("submit", (e) => {
     addOrUpdateTask();
 });
 
-const myTaskArr = [
-    { task: "Walk the Dog", date: "22-04-2022" },
-    { task: "Read some books", date: "02-11-2023" },
-    { task: "Watch football", date: "10-08-2021" },
-];
+// Step 60
+// Remove the myTaskArr array and all of the code for localStorage because you
+// don't need them anymore.
 
-localStorage.setItem("data",JSON.stringify(myTaskArr));
-const getTaskArr = localStorage.getItem("data")
-console.log(getTaskArr)
+// const myTaskArr = [
+//     { task: "Walk the Dog", date: "22-04-2022" },
+//     { task: "Read some books", date: "02-11-2023" },
+//     { task: "Watch football", date: "10-08-2021" },
+// ];
+
+// localStorage.setItem("data",JSON.stringify(myTaskArr));
+
+// Using localStorage.clear() won't just delete a single item from local storage
+// but will remove all items. 
+// localStorage.removeItem("data");
+// localStorage.clear();
+
+// const getTaskArr = localStorage.getItem("data");
+// console.log(getTaskArr);
+
+// const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+// console.log(getTaskArrObj);
