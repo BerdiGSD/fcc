c3ea6dce13d835ac0fd230e89cc6fd3db16497c0
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2025-01-21 07:16:11 -0600

    Working with regex to determine if a message is spam

--- jsProjects/SpamFilter/script.js
+++ jsProjects/SpamFilter/script.js
@@ -2,9 +2,23 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+// const isSpam = (msg) => msg.match(helpRegex);
+const isSpam = (msg) => helpRegex.test(msg); 
+//Instead of using the .match() method, you can use the .test() method of a
+//regular expression to test if a string matches the pattern.
 
 checkMessageButton.addEventListener("click",() => {
+    activateEventListener();
+});
+
+messageInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        activateEventListener()
+    };
+});
+
+const activateEventListener = () => {
     if (messageInput.value === "" ) {
         alert("Please enter a message.");
         return;
@@ -14,4 +28,4 @@ checkMessageButton.addEventListener("click",() => {
     "Oh no! This looks like a spam message." : "This message does not seem to contain any spam."
 
     messageInput.value = "";
-});
+}
