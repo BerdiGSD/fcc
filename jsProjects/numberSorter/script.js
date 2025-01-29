7fdd6cf67a398f480a24c04efd206f61a31b898f
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2025-01-28 20:56:47 -0600

    making my way downtown, walking fast, making my code work

---
+++ jsProjects/numberSorter/script.js
@@ -0,0 +1,8 @@
+const sortButton =  document.getElementById("sort");
+const sortInputArray = (event) => {
+    event.preventDefault();
+
+    
+};
+
+sortButton.addEventListener("click",sortInputArray);
