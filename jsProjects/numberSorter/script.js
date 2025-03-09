a345e4efa614df7cab74b14240b2048a2747beb1
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2025-03-08 19:31:30 -0600

    completed lesson on bubble sort, selection sort, and insertion sort. Still have much to learn

--- jsProjects/numberSorter/script.js
+++ jsProjects/numberSorter/script.js
@@ -9,15 +9,17 @@ const sortInputArray = (event) => {
 
     // const sortedValues = bubbleSort(inputValues); // Utilizing bubble sort
     // const sortedValues = selectionSort(inputValues); // Utilizing selection sort
-    // const sortedValues = selectionSort_fireship(inputValues); // Utilizing selection sort fireship version
-    const sortedValues = insertionSort(inputValues); // Utilizing Insertion sort
+    // const sortedValues = selectionSortFireShipVersion(inputValues); // Utilizing selection sort fireship version
+    // const sortedValues = insertionSort(inputValues); // Utilizing Insertion sort
+    const sortedValues = inputValues.sort((a, b) => { return a - b });
     updateUI(sortedValues);
+
 };
 
 const updateUI = (array = []) => {
     array.forEach((num, i) => {
         const outputValueNode = document.getElementById(`output-value-${i}`);
-        outputValueNode.innerText = num.value;
+        outputValueNode.innerText = num;
     })
 };
 
@@ -53,25 +55,24 @@ const selectionSort = (array) => {
 };
 
 
-const selectionSort_fireship = (arr) => {
-    console.log(arr)
+const selectionSortFireShipVersion = (arr) => {
+    console.log("Initial array: " + arr)
     for (let i = 0; i < arr.length; i++) {
         let min = i;
-        console.log("first loop i: " + min)
-        console.log(arr)
+        console.log("first loop i: " + arr[min])
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[min]) {
+                console.log(`array[${arr[j]}] < array[${min}]`);
                 console.log("IF arr[j] < arr[min]: " + (arr[j] < arr[min]));
                 min = j;
-                console.log("Second loop j: " + min);
+                console.log("Second loop j: " + arr[min]);
             }
-            console.log(arr)
         }
         if (min !== i) {
+            console.log(`min being ${min} and i being ${i}`);
             console.log("IF min !== i: " + (min !== i));
             [arr[i], arr[min]] = [arr[min], arr[i]];
             console.log(`Swap: [${arr[i]}, ${arr[min]}] = [${arr[min]}, ${arr[i]}]`);
-            console.log(arr)
         }
     }
     console.log("Final: " + arr)
@@ -82,12 +83,15 @@ const insertionSort = (array) => {
     for (let i = 1; i < array.length; i++){
         const currValue = array[i];
         let j = i - 1;
+        //Find the correct position for currValue
         while (j >= 0 && array[j] > currValue) {
             array[j + 1] = array[j]; //shift larger value to the right
-            j--;
+            j--; //move to the next element on the left
         };
-
+        // Insert currValue at the correct position
+        array[j + 1] = currValue;
     }
+    return array; // Return the sorted array
 };
 
 sortButton.addEventListener("click",sortInputArray);
