4580384b7a0d5f4c627de3e389596414e9b33e21
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2025-03-10 14:53:13 -0500

    completed median function, next mode

--- jsProjects/advStatsCalculator/readme.md
+++ jsProjects/advStatsCalculator/readme.md
@@ -55,5 +55,17 @@ const getMean = (array) => {
     return mean;
 };
 */
-// However there is a way to implicitly return the mean of the array
+// However there is a way to implicitly return the mean of the array:
 const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length; 
+
+~NOTES~
+// Test code for finding median: 
+
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
+console.log(evenListMedian);
--- jsProjects/advStatsCalculator/script.js
+++ jsProjects/advStatsCalculator/script.js
@@ -1,10 +1,23 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length; 
 
+const getMedian = (array) => {
+    const sorted = array.toSorted((a, b) => a - b);
+    if (sorted.length % 2 === 0) {
+        return getMean([sorted[sorted.length / 2 - 1], sorted[sorted.length / 2]]);
+    } else {
+        return sorted[Math.floor(sorted.length / 2)];
+    }
+};
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
     
     const mean = getMean(numbers);
-    document.querySelector("#mean").textContent = mean; 
+    document.querySelector("#mean").textContent = mean;
+    
+    const median = getMedian(numbers);
+    document.querySelector("#median").textContent = median;
 }
+
